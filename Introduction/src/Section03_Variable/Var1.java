4edebdef3119b649e230d1421e688d82a7cee138
ssohyun0 <jsh0312033@gmail.com>
2025-03-27 15:31:37 +0900

    add section03

---
+++ Introduction/src/Section03_Variable/Var1.java
@@ -0,0 +1,8 @@
+package Section03_Variable;
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(20);
+        System.out.println(30);
+    }
+}
