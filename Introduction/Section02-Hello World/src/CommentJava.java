4b63e3bdfdf6804c243a7cf235f59ca5e198a1b0
ssohyun0 <jsh0312033@gmail.com>
2025-03-27 14:10:06 +0900

    Initial commit

---
+++ Introduction/Section02-Hello World/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) { //psvm 입력
+        System.out.println("hello java1"); //sout 입력
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+
+    }
+}
