d00777213ee4eef0c1afa193f3bd79afde50d1a9
ssohyun0 <jsh0312033@gmail.com>
2025-04-10 15:41:21 +0900

    add section03

---
+++ Introduction/src/Section03_Variable/Var6.java
@@ -0,0 +1,14 @@
+package Section03_Variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a);
+        // java: variable a might not have been initialized
+        // 초기화하지 않으면 이상한 값이 출력될 수 있기에 자바는 변수를 초기화 하도록 강제함
+
+        // 지역 변수는 개발자가 직접 초기화해야 함
+        // 클래스 변수와 인스턴스 변수는 자바가 자동으로 초기화를 진행
+    }
+}
+
---
+++ Introduction/src/Section03_Variable/Var7.java
@@ -0,0 +1,21 @@
+package Section03_Variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; //불리언(boolean) true, false 입력 가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; // 문자열, 문자열을 다루기 위한 특별한 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        // ** 단축키 : command + d **
+
+        // 리터럴(literal) : 코드에서 개발자가 직접 적은 고정된 값
+        // ex. 100, 10,5, true ..
+    }
+}
