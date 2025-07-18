624b230a03dd132ccdffb6222823cc52e88ea88f
ssohyun0 <jsh0312033@gmail.com>
2025-07-18 16:54:53 +0900

    Create Basic & Section01 Completed

---
+++ Basic/src/section01_Class1/Student.java
@@ -0,0 +1,7 @@
+package section01_Class1;
+
+public class Student {
+    String name;
+    int age;
+    int grade;
+}
---
+++ Basic/src/section01_Class1/ex/MovieReview.java
@@ -0,0 +1,6 @@
+package section01_Class1.ex;
+
+public class MovieReview {
+    String title;
+    String review;
+}
